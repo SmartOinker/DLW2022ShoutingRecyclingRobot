c21985d868b7ee2d772c2969d77b78b4261f2f18
yuyouyou032 <69706398+yuyouyou032@users.noreply.github.com>
2022-10-03 10:48:11 +0800

    updated qr_detection file

--- qr_detection.py
+++ qr_detection.py
@@ -7,17 +7,21 @@ import webbrowser
 
 cap = cv2.VideoCapture(0)
 detector = cv2.QRCodeDetector()
-
+bin_id = 1
 def scan_qr():
     _, img = cap.read()
-    data, bbox, _ = detector.detectAndDecode(img)
+    try:
+        data, bbox, _ = detector.detectAndDecode(img)
+    except:
+        print("no qr. continue")
+        return 0
     #check if there is a QRCode in the image
     if data:
+        print("scanned.")
         id=data
-        b=webbrowser.open(str(id))
         cap.release()
         print(data)
-        return id
+        return data
 
        
     if cv2.waitKey(1) == ord("q"):
@@ -27,11 +31,12 @@ def scan_qr():
 
 
 def reward(id):
-	id = str(id)
-	requests.get(f"https://cookiespeanutbutter.herokuapp.com/add/{id}/5")
-	return id
+    id = str(id)
+    print(f"id: {id}")
+    req = requests.get(f"https://cookiespeanutbutter.herokuapp.com/add/{id}/{bin_id}")
+    print(req.text)
+    return id
 	
 while True:
     id = scan_qr()
     reward(id)
-    time.sleep(2)
